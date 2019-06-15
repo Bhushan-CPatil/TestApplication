fb276a4fb428ba0db6fd1ff7d30f738ce8d26ec1
Lenovo <pbhushan246@gmail.com>
2019-06-15 08:40:21 +0530

    Initial commit

--- app/src/main/java/com/bhushan/testapplication/activity/TicketBooking.java
+++ app/src/main/java/com/bhushan/testapplication/activity/TicketBooking.java
@@ -3,6 +3,8 @@ package com.bhushan.testapplication.activity;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,7 +14,10 @@ public class TicketBooking extends AppCompatActivity {
 
     TextView date,from,to,arrival,depar,cost,airline,modelname,totseats,duration,qty,price;
     ImageView minus,plus;
-    int totprice;
+    Button next;
+    int totprice,totalseats,amount;
+    int minteger = 1;
+    String s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +37,7 @@ public class TicketBooking extends AppCompatActivity {
         minus = findViewById(R.id.minus);
         plus = findViewById(R.id.plus);
         price = findViewById(R.id.price);
+        next = findViewById(R.id.next);
 
         date.setText("Date : " + getIntent().getStringExtra("fDATE"));
         from.setText(getIntent().getStringExtra("f_FROM"));
@@ -43,9 +49,48 @@ public class TicketBooking extends AppCompatActivity {
         modelname.setText(getIntent().getStringExtra("fMODEL"));
         totseats.setText(getIntent().getStringExtra("tOTSEAT"));
         duration.setText(getIntent().getStringExtra("f_DURATION"));
-        qty.setText("1");
-        price.setText("₹. "+getIntent().getStringExtra("eCOST"));
+        qty.setText(""+minteger);
+        totalseats = Integer.parseInt(getIntent().getStringExtra("tOTSEAT"));
+        amount = Integer.parseInt(getIntent().getStringExtra("eCOST"));
+        totprice = minteger * Integer.parseInt(getIntent().getStringExtra("eCOST"));
+        price.setText("Total Price: ₹. "+totprice);
 
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(minteger<totalseats) {
+                    minteger = minteger + 1;
+                    qty.setText(String.valueOf(minteger));
+                    int cal2 = amount * minteger;
+                    s = String.valueOf(cal2);
+                    price.setText("Total Price: ₹ "+s);
+                }
+
+            }
+        });
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(minteger>1) {
+                    minteger = minteger - 1;
+                    qty.setText(String.valueOf(minteger));
+                    int cal2 = amount * minteger;
+                    s = String.valueOf(cal2);
+                    price.setText("Total Price: ₹ "+s);
+                }
+                else {
+                    qty.setText("1");
+                }
+
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
